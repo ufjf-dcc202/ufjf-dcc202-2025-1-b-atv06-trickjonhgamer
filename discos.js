f7b2e6cdda529c388ec8bbcb66da9d95df08a958
trickjonhgamer <trickjonhgamer@gmail.com>
2025-08-14 11:17:00 -0300

    cria a função seleciona necessaria para mover a peça e a variavel selecionado.

--- discos.js
+++ discos.js
@@ -1,9 +1,21 @@
 const tabuleiro = [
-    null,null,"A","A","A",null,null,
+    null, null, "A", "A", "A", null, null,
     null, null, "A", "A", "A", null, null,
     "A", "A", "A", "A", "A", "A", "A",
     "A", "A", "A", "T", "A", "A", "A",
     "A", "A", "A", "A", "A", "A", "A",
     null, null, "A", "A", "A", null, null,
     null, null, "A", "A", "A", null, null
-]
+];
+let selecionado = null; 
+export function seleciona(posicao) {
+    if (selecionado === null) {
+      selecionado = posicao;
+    } else if (selecionado === posicao) {
+      selecionado = null;
+    } else {
+      mover(selecionado, posicao);
+      selecionado = null;
+    }
+    console.log(tabuleiro);
+  }
