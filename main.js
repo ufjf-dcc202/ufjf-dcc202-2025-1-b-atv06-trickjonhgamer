dd2a00f0aaaaaca3e56cc4629df50d3b4e738298
trickjonhgamer <trickjonhgamer@gmail.com>
2025-08-14 11:55:33 -0300

    renomeia o html corretamente e adciona o codigo que cria o tabuleiro e adciona discos a ele nas posições necessárias

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import { getTabuleiro, seleciona } from "./discos.js";
+const eH1 = document.querySelector("h1");
+eH1.textContent = "Olá Mundo!";
+
+const eTabuleiro = criaTabuleiro();
+document.body.append(eTabuleiro);
+const tabuleiro = getTabuleiro();
+for (let i = 0; i < tabuleiro.length; i++) {
+for (let j = 0; j < tabuleiro[i].length; j++) {
+if (tabuleiro[i][j] !== null) {
+const eDisco = criaDisco(tabuleiro[i][j], i, j);
+eTabuleiro.children[i].children[j].append(eDisco);
+    }
+  }
+}
+function atualizaDiscos() {
+    const tabuleiro = getTabuleiro();
+    for (let i = 0; i < eTabuleiro.children.length; i++) {
+      const disco = eTabuleiro.children[i];
+      disco.dataset.cor = tabuleiro[disco.dataset.posicao];
+    }
+  }
