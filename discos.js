66720649f8ceb5ed7dc30a15cc7455e5fa25bf2b
trickjonhgamer <trickjonhgamer@gmail.com>
2025-08-14 11:07:11 -0300

    cria o tabuleiro em discos

--- discos.js
+++ discos.js
@@ -1,3 +1,9 @@
 const tabuleiro = [
-    
+    null,null,"A","A","A",null,null,
+    null, null, "A", "A", "A", null, null,
+    "A", "A", "A", "A", "A", "A", "A",
+    "A", "A", "A", "T", "A", "A", "A",
+    "A", "A", "A", "A", "A", "A", "A",
+    null, null, "A", "A", "A", null, null,
+    null, null, "A", "A", "A", null, null
 ]
