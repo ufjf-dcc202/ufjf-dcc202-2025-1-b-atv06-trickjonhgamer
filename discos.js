81e2578bb2ecf4a19f53984e42055fe7efaea75b
trickjonhgamer <trickjonhgamer@gmail.com>
2025-08-14 21:28:39 -0300

    copia o trablaho anterior que funciona para tentar fazer o atual atraves de alteraçoes  neste

---
+++ discos.js
@@ -0,0 +1,40 @@
+const tabuleiro = [
+"Laranja",
+"Laranja",
+"Laranja",
+"Transparente",
+"Amarelo",
+"Amarelo",
+"Amarelo",
+];
+let selecionado = null;
+export function seleciona(posicao) {
+if (selecionado === null) {
+selecionado = posicao;
+} else if (selecionado === posicao) {
+selecionado = null;
+} else {
+mover(selecionado, posicao);
+selecionado = null;
+}
+console.log(tabuleiro);
+}
+export function getTabuleiro() {
+return [...tabuleiro];
+}
+function mover(origem, destino) {
+if (tabuleiro[destino] !== "Transparente") {
+return;
+}
+if (Math.abs(destino - origem) > 2) {
+return;
+}
+if (destino - origem === 2 && tabuleiro[origem + 1] === "Transparente") {
+return;
+}
+if (destino - origem === -2 && tabuleiro[origem - 1] === "Transparente") {
+return;
+}
+tabuleiro[destino] = tabuleiro[origem];
+tabuleiro[origem] = "Transparente";
+}
---
+++ main.js
@@ -0,0 +1,40 @@
+import { getTabuleiro, seleciona } from "./discos.js";
+
+const eH1 = document.querySelector("h1");
+eH1.textContent = "Olá Mundo!";
+
+const eTabuleiro = criaTabuleiro();
+document.body.append(eTabuleiro);
+const tabuleiro = getTabuleiro();
+for (let i = 0; i < 7; i++) {
+  const eDisco = criaDisco(tabuleiro[i], i);
+  eTabuleiro.append(eDisco);
+}
+function atualizaDiscos() {
+  const tabuleiro = getTabuleiro();
+  for (let i = 0; i < eTabuleiro.children.length; i++) {
+    const disco = eTabuleiro.children[i];
+    disco.dataset.cor = tabuleiro[disco.dataset.posicao];
+  }
+}
+function discoClick(evento) {
+  const posicao = Number(evento.target.dataset.posicao);
+  console.log("Click" + posicao);
+  seleciona(posicao);
+  atualizaDiscos();
+}
+
+function criaDisco(cor, posicao) {
+  const novoDisco = document.createElement("div");
+  novoDisco.classList.add("disco");
+  novoDisco.dataset.cor = cor;
+  novoDisco.dataset.posicao = posicao;
+  novoDisco.addEventListener("click", discoClick);
+  return novoDisco;
+}
+
+function criaTabuleiro() {
+  const novoTabuleiro = document.createElement("div");
+  novoTabuleiro.classList.add("tabuleiro");
+  return novoTabuleiro;
+}
